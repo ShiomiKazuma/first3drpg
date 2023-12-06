c1f526df5aab6f9267e24f415d7e48260bf6b69f
ShiomiKazuma <kazu_shio_1217@icloud.com>
2023-12-06 23:25:30 +0900

    [add]ステータスマネージャーを追加

---
+++ Assets/Script/StatusManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusManager : MonoBehaviour
+{
+    [SerializeField] GameObject _main;
+    [SerializeField] int _currentHp;
+    [SerializeField] int _maxHp;
+    [SerializeField] GameObject _effect;
+    [SerializeField] float _effectTime = 3.0f;
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip _hitSE;
+    [SerializeField] string _tagName;
+
+    private void Update()
+    {
+        if(_currentHp <= 0)
+        {
+            _currentHp = 0;
+            var effect = Instantiate(_effect);
+            effect.transform.position = transform.position;
+            Destroy(effect, _effectTime);
+            this.enabled = false;
+            Destroy(_main.gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == _tagName)
+        {
+            Damage();
+        }
+    }
+
+    void Damage()
+    {
+        _audioSource.PlayOneShot(_hitSE);
+        _currentHp--;
+    }
+}
